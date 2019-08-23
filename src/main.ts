19eb5143c4d13374bd30553ce0d92a7c2eaf25ac
zhuchangfeng <798928604@qq.com>
2019-08-23 16:47:11 +0800

    vue+TS第一次提交

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+// tslint:disable-next-line:no-consecutive-blank-lines
+
+// tslint:disable-next-line:no-unused-expression
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});
